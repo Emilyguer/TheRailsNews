d1325ac99722e4b27c69752a0b650a1e3d567b89
Emiguer <emilyguerrero@gmail.com>
2023-08-25 11:01:22 -0400

    Feat:(Confirguaraciones al user)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+    before_action :configure_sign_up_params, only: [:create]
+    before_action :configure_account_update_params, only: [:update]
+
+
+  protected
+  def configure_sign_up_params
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :phone])
+  end
+  def configure_account_update_params
+  devise_parameter_sanitizer.permit(:account_update, keys: [:name, :age,
+  :phone])
+  end
+
 
   # GET /resource/sign_up
   # def new
