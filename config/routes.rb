0466f43798d0fac3f9c8482408639767b8420947
Emiguer <emilyguerrero@gmail.com>
2023-08-25 10:36:56 -0400

    Feat:(User Views y controllers) se instalo las vistas y controlar de divise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+   }
+   
+
+  
  #vistas estaticas
   
  get 'home', to: 'pages#home', as: 'home'
