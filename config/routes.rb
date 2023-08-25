cc7ac74946480373026455544fb56f48c3fe2b7d
Emiguer <emilyguerrero@gmail.com>
2023-08-25 09:35:09 -0400

    Feat:(Pages Routes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/term'
-  get 'pages/privacy'
-  get 'pages/copyright'
+ #vistas estaticas
+  
+ get 'home', to: 'pages#home', as: 'home'
+ get 'term', to: 'pages#term', as: 'term'
+ get 'privacy', to: 'pages#privacy', as: 'privacy'
+ get 'copyright', to: 'pages#copyright', as: 'copyright'
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+ #ruta raiz
+  root "pages#home"
 end
