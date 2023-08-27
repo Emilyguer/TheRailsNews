8f65024212664be981d4053eb7f614927627dcf7
Emiguer <emilyguerrero@gmail.com>
2023-08-27 04:39:47 -0400

    Feat:(configuraciones finales)

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -16,4 +16,10 @@ class RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
+
+private
+
+def sign_up_params
+  params.require(:user).permit(:email, :password, :password_confirmation, :role)
+end
 end
